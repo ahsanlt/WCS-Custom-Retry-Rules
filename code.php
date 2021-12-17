40fdbfdda28644ef13ae0425f3b3e69788ab20cc
ahsanlt <55467917+ahsanlt@users.noreply.github.com>
2021-12-18 03:11:26 +0500

    Create code.php

---
+++ code.php
@@ -0,0 +1,43 @@
+<?php
+
+
+function custom_retry_rules( $default_retry_rules_array ) {
+    return array(
+            array(
+                'retry_after_interval'            => 1 * DAY_IN_SECONDS,
+                'email_template_customer'         => '',
+                'email_template_admin'            => 'WCS_Email_Payment_Retry',
+                'status_to_apply_to_order'        => 'pending',
+                'status_to_apply_to_subscription' => 'active',
+            ),
+            array(
+                'retry_after_interval'            => 3 * DAY_IN_SECONDS,
+                'email_template_customer'         => 'WCS_Email_Customer_Payment_Retry', 
+                'email_template_admin'            => '',
+                'status_to_apply_to_order'        => 'pending',
+                'status_to_apply_to_subscription' => 'active',
+            ),
+            array(
+                'retry_after_interval'            => WEEK_IN_SECONDS,
+                'email_template_customer'         => '', 
+                'email_template_admin'            => '',
+                'status_to_apply_to_order'        => 'pending',
+                'status_to_apply_to_subscription' => 'on-hold',
+            ),
+            array(
+                'retry_after_interval'            => 10 * DAY_IN_SECONDS,
+                'email_template_customer'         => 'WCS_Email_Customer_Payment_Retry', 
+                'email_template_admin'            => '',
+                'status_to_apply_to_order'        => 'pending',
+                'status_to_apply_to_subscription' => 'on-hold',
+            ),
+            array(
+                'retry_after_interval'            => 14 * DAY_IN_SECONDS,
+                'email_template_customer'         => '', 
+                'email_template_admin'            => 'WCS_Email_Payment_Retry',
+                'status_to_apply_to_order'        => 'pending',
+                'status_to_apply_to_subscription' => 'on-hold',
+            ),
+        );
+}
+add_filter( 'wcs_default_retry_rules', 'custom_retry_rules' );
